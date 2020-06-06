45bb38a6300eac6e05da8c3a7dada24505b04bd6
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-06-06 17:35:19 -0500

    password generator - still needs polish

--- Develop/script.js
+++ Develop/script.js
@@ -18,15 +18,39 @@
 var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var symbolsArray = ["!", "@", "#", "$", "%", "&", "|", ";", ">", "~", "`"]
-var randomNum = Math.floor(Math.random() * 9) + 1;
+var numberArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+// Thought it would be fun to use this, but I don't think it'll work.
+// randomNum = Math.floor(Math.random() * 9) + 1;
 var answersArray = []
 var passwordArray = []
 
 // var allOptionsArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "[", "@", "^", "$", ".", "|", "?", "*", "+", "(", ")", "]", "%", "~", "!"]
 // var allLettersArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
+// Alternative solution: all inputs should be its own function 
+
 
 var generatePassword = function () {
+  var includeLowercase;
+  var includeUppercase;
+  var includeNum;
+  var includeSymbols;
+
+  function inputs() {
+    includeLowercase = window.confirm("Would you like your password to include lowercase letters?");
+    console.log("Include lowercase letters:", includeLowercase);
+
+    includeUppercase = window.confirm("Would you like your password to include uppercase letters?");
+    console.log("Include uppercase letters:", includeUppercase);
+
+    includeNum = window.confirm("Would you like your password to include numbers?");
+    console.log("Include numbers:", includeNum);
+
+    includeSymbols = window.confirm("Would you like your password to include symbols?");
+    console.log("Include symbols:", includeSymbols);
+  }
+
   var pwLength = window.prompt("How long do you need your password to be? Please enter a value between 8 and 128.");
   console.log("This many char:", pwLength);
 
@@ -34,64 +58,55 @@ var generatePassword = function () {
     window.alert("Ahem... please select a value between 8 and 128!");
     generatePassword();
   }
+
   else {
-    var includeLowercase = window.confirm("Would you like your password to include lowercase letters?");
-    console.log("Include lowercase letters:", includeLowercase);
-
-    var includeUppercase = window.confirm("Would you like your password to include uppercase letters?");
-    console.log("Include uppercase letters:", includeUppercase);
-
-    var includeNum = window.confirm("Would you like your password to include numbers?");
-    console.log("Include numbers:", includeNum);
-
-    var includeSymbols = window.confirm("Would you like your password to include symbols?");
-    console.log("Include symbols:", includeSymbols);
-
+    inputs();
   }
 
+  if (includeLowercase === false && includeUppercase === false && includeNum === false && includeSymbols === false) {
+    window.alert("You must select at least one type of character.");
+    inputs();
+  }
 
   if (includeLowercase === true || includeUppercase === true || includeNum === true || includeSymbols === true) {
     answersArray.push(includeLowercase, includeUppercase, includeSymbols, includeNum);
     console.log(answersArray);
-
-  } else {
-    window.alert("You must select at least one type of character.");
   }
 
 
   while (passwordArray.length < pwLength) {
     // If user wants LOWERCASE LETTERS, this pulls at least one.
-    if (answersArray[0] === true) {
+    if (answersArray[0] === true && passwordArray.length < pwLength) {
       passwordArray.push(lowercaseArray[Math.floor(Math.random() * 26)]);
       console.log(passwordArray);
     }
 
     // If user wants UPPERCASE LETTERS, this pulls at least one.
-    if (answersArray[1] === true) {
+    if (answersArray[1] === true && passwordArray.length < pwLength) {
       passwordArray.push(uppercaseArray[Math.floor(Math.random() * 26)]);
       console.log(passwordArray);
     }
 
     // If user wants NUMBERS, this pulls at least one.
-    if (answersArray[2] === true) {
-      passwordArray.push(randomNum);
+    if (answersArray[2] === true && passwordArray.length < pwLength) {
+      passwordArray.push(numberArray[Math.floor(Math.random() * 9)]);
       console.log(passwordArray);
     }
 
     // If user wants SYMBOLS, this pulls at least one.
-    if (answersArray[3] === true) {
+    if (answersArray[3] === true && passwordArray.length < pwLength) {
       passwordArray.push(symbolsArray[Math.floor(Math.random() * 11)]);
-      console.log(passwordArray);
+      console.log(passwordArray.join(""));
     }
   }
+
+  // password = passwordArray.join(",");
 
   // Loop the above WHILE the pwLength value is < the length of passwordArray
   // Then once the length of passwordArray = pwLength, convert passwordArray to a string
   // That string becomes 'password' ???
 
 }
-
-
 
 
 // Get references to the #generate element
@@ -102,8 +117,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+  passwordText.value = passwordArray.join("");
 }
 
 // Add event listener to generate button
